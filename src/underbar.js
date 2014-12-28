5110efe5abdebcd0809f600dbc5bf686b4a0fec8
Ryan Atkinson <rjatkinson2@gmail.com>
2014-12-28 16:25:53 -0500

    Completed _.invoke function definition. This is a really lousy implementation for a number of reasons. For starters, I could not find an elegant way to call methods without validating what type of object the first value is in either the object or array, and then calling the method on that type's prototype. Therefore, mixed type arrays with all strings but starting with a single number will fail for a string method across the board because the function will call Number.prototype.<method name> for every value. Secondly, I haven't implemented the args argument to allow methods that take arguments. Will finish this after all pre-course work is done.

--- src/underbar.js
+++ src/underbar.js
@@ -355,7 +355,15 @@
 
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
-  _.invoke = function (collection, functionOrKey, args) {};
+  _.invoke = function (collection, functionOrKey, args) {
+    var proto = (typeof collection[Object.keys(collection)[0]]);
+    var meth = (proto==='string') ? String.prototype[functionOrKey] : ((proto==='number') ? Number.prototype[functionOrKey] : Boolean.prototype[functionOrKey]);
+    var func = ((typeof functionOrKey) === 'string') ? meth : functionOrKey;
+    _.each(collection, function(item,indexOrKey,collection){
+      collection[indexOrKey] = func.call(item);
+    });
+    return(collection);
+  };
 
   // Sort the object's values by a criterion produced by an iterator.
   // If iterator is a string, sort objects by that property with the name
