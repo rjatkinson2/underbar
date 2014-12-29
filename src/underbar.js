0448b2e1e2c2fd59495c85dee624c9cebd99c72e
Ryan Atkinson <rjatkinson2@gmail.com>
2014-12-28 19:58:06 -0500

    Completed _.sortBy function definition. This marks the completion of all test cases for both the required and extra credit portions of the underbar assignment!

--- src/underbar.js
+++ src/underbar.js
@@ -369,7 +369,16 @@
   // If iterator is a string, sort objects by that property with the name
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
-  _.sortBy = function (collection, iterator) {};
+  _.sortBy = function (collection, iterator) {
+    if (typeof iterator === 'string') {
+      return collection.sort(function (a, b) {
+        return a[iterator] - b[iterator];
+      });
+    }
+    return collection.sort(function (a, b) {
+      return iterator(a) - iterator(b);
+    });
+  };
 
   // Zip together two or more arrays with elements of the same index
   // going together.
